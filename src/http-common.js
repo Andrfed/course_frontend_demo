0a35d0c3b4146771814ef7f7ecd3c55e0855b024
Aleksey Nasibulin <alealexpro100@ya.ru>
2022-12-11 18:27:08 +0800

    fix auth

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 var token = "";
-var user = JSON.parse(localStorage.getItem('user'));
+var user;
+if (localStorage.getItem('isAdmin')=="true") {
+    user = JSON.parse(localStorage.getItem('admin'));
+} else {
+    user = JSON.parse(localStorage.getItem('user'));
+}
 if (user && user.accessToken) {
     token = user.accessToken;
-
 }
 export default axios.create({
     baseURL: "http://localhost:3000/api", // указание адреса сервера
